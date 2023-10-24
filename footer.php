4d94ab0b35b84c715f46cd31b6c046447847e4e2
Hind-HTH <h.toutaoui96@gmail.com>
2023-10-24 12:26:11 +0200

    footer (second-V)

--- footer.php
+++ footer.php
@@ -12,6 +12,8 @@
 
     <link rel="stylesheet"
     href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
 
 </head>
 
@@ -31,28 +33,26 @@
         </div>
 
         <div class="footer-content">
-            <h4>Projects</h4>
-            <li><a href="#">Houses</a></li>
-            <li><a href="#">Rooms</a></li>
-            <li><a href="#">Flats</a></li>
-            <li><a href="#">Appartement</a></li>
+            <h4>Informations Pratiques</h4>
+            <li><a href="contact.php"><i class="fas fa-location-dot"></i> Adress:</br>  20 rue cail 75010 Paris</a></li>
+            <li><a href="#"><i class="fas fa-mobile-alt"></i> Numtelephone:</br> +33 0123456789</a></li>
+            <li><a href="#"><i class="fas fa-phone"></i> telephonefix: </br> +33 0123456789</a></li>
+            <!-- <li><a href="contact.php"><i class="fas fa-envelope"></i> Envoyer nous un email</a></li> -->
+            
         </div>
 
         <div class="footer-content">
-            <h4>Company</h4>
-            <li><a href="#">How we work</a></li>
-            <li><a href="#">Capital</a></li>
-            <li><a href="#">Security</a></li>
-            <li><a href="#">Sellings</a></li>
+            <h4>Asistance</h4>
+            <li><a href="contact.php"><i class="fas fa-envelope"></i> Envoyer nous un email</a></li>
         </div>
 
-        <div class="footer-content">
+        <!-- <div class="footer-content">
             <h4>Movement</h4>
             <li><a href="#">Movement</a></li>
             <li><a href="#">Support Us</a></li>
             <li><a href="#">Parcing</a></li>
             <li><a href="#">Renting</a></li>
-        </div>
+        </div> -->
 
         <div class="footer-content">
             <h4>Help</h4>
@@ -62,7 +62,12 @@
             <li><a href="#">FAQs</a></li>
         </div>
 
+        <div class="footer-content logo-container">
+            <img src="./Assets/images/img3.jpg" class="logo_img" style="height: auto; width:auto;">
+        </div>
+
     </section>
 </body>
+
 
 </html>
